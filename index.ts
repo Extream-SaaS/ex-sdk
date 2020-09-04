c29c4ded5ec431f4d791a0da2f94dae8b2ae1e69
Chris Saganic <chris@saganic.co.uk>
2020-09-04 18:02:25 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+// @ts-ignore
+import io from 'socket.io-client'
+
+export type options = {
+  auth: string
+  gateway: string
+  apiKey: string
+}
+export type headers = {
+  'Content-Type': string
+  Authorization: string
+}
+
+export class ExtremeClient {
+  options: options;
+  io: any
+
+  constructor (options: options) {
+    this.options = options
+    this.io = io
+  }
+
+  private headers: headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Basic ${this.options.apiKey}`
+  };
+
+  /**
+   * Given a username and password, will authenticate the user against the ExtremeClient
+   *
+   * @param { string } username
+   * @param { string } password
+   * @returns { Promise<any> }
+   *
+   */
+  public async auth (username: string, password: string): Promise<any> {
+    try {
+      const auth = await fetch(`${this.options.auth}/auth/login?username=${username}`,
+        {
+          method: 'GET',
+          headers: this.headers
+        }
+      )
+      const resp = await auth.json()
+      return resp
+    } catch (error) {
+      return error
+    }
+  }
+}
