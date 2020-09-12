a08f25669dcafe57140180653e9bef95d6fd037b
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-09-12 14:47:19 +0100

    docs: some parts docs

--- src/subscription-manager.ts
+++ src/subscription-manager.ts
@@ -1,4 +1,10 @@
 /* eslint-disable */
+/**
+ * Manages a list of subscriptions, holding references to the listeners attached to allow classes to clean
+ * up all the listeners they setup on creation easily.
+ *
+ * Consumers of this class should create their own subscription managers in constructors and expose a destory method
+ */
 export default class SubscriptionManager {
   private socket: SocketIOClient.Socket
   private listeners: { key: string, listener: (...args: any[]) => void }[] = []
@@ -7,6 +13,11 @@ export default class SubscriptionManager {
     this.socket = socket
   }
 
+  /**
+   * List to for an event on the websocket
+   * @param {string} event event name
+   * @param {(...args: any[]) => void)} callback function to be called on event fire
+   */
   addSubscription (event: string, callback: (...args: any[]) => void): void {
     this.listeners.push({
       key: event,
@@ -15,6 +26,9 @@ export default class SubscriptionManager {
     this.socket.on(event, callback)
   }
 
+  /**
+   * Clean up all subscription the manager has setup
+   */
   removeAllSubscriptions (): void {
     this.listeners.forEach(({ key, listener }) => {
       this.socket.removeEventListener(key, listener)
