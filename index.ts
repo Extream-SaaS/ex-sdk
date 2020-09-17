8a5b9aef7ddc44b8b3b8c08eb00cdba0b09dccc6
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-09-17 22:03:41 +0100

    fix: stupid error

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import io from 'socket.io-client'
-import { ExtreamAuthUser } from './types/user'
+import { ExtreamAuthUser } from './src/user'
 import { Admin as AdminActions, Admin } from './src/admin'
 import { Consumer as ConsumerActions, Consumer } from './src/consumer'
 import { Client as ClientActions, Client } from './src/client'
