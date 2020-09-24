eaf65fb05ceac8bc679bf1478bf9e10c58cbb0fa
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-09-24 21:41:52 +0100

    test: knocking em down 2/?

--- src/__tests__/chat.spec.ts
+++ src/__tests__/chat.spec.ts
@@ -243,7 +243,7 @@ describe('Chat class', () => {
     expect(chat.messages[2].uuid).toBe('id2')
   })
 
-  it('adds a message if streamed in', async () => {
+  test('adds a message if streamed in', async () => {
     const joinPromise = chat.join()
     socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
     socket.socketClient.emit(ConsumerTopic.ChatGet, chatResponse)
@@ -287,7 +287,7 @@ describe('Chat class', () => {
     }])
   })
 
-  it('adds a child message if streamed in', async () => {
+  test('adds a child message if streamed in', async () => {
     const joinPromise = chat.join()
     socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
     socket.socketClient.emit(ConsumerTopic.ChatGet, chatResponse)
@@ -300,7 +300,7 @@ describe('Chat class', () => {
       sent: '2020-09-24T19:54:05.940Z',
       uuid: 'id4'
     })
-    console.log('%o', chat.messages)
+
     expect(chat.messages).toStrictEqual([{
       from: {
         email: 'foo@bar.com',
@@ -333,37 +333,210 @@ describe('Chat class', () => {
     }])
   })
 
-  // it('removes a message if streamed in', () => {
-  //   expect(false).toBeTruthy()
-  // })
+  test('removes a message if streamed in', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, chatResponse)
+    await joinPromise
+    socket.socketClient.emit(ConsumerTopic.ChatRemove, {
+      id: 'roomId',
+      uuid: 'id1'
+    })
 
-  // it('removes a child message if streamed in', () => {
-  //   expect(false).toBeTruthy()
-  // })
+    expect(chat.messages).toStrictEqual([{
+      from: {
+        email: 'foo@bar.com',
+        id: '1',
+        token: 'token',
+        user_type: 'audience',
+        username: 'baz'
+      },
+      message: 'Some text',
+      sent: '2020-09-23T00:00:17.047Z',
+      uuid: 'id1',
+      children: [],
+      removed: true
+    }])
+  })
 
-  // it('does not resolve if the get message is not the same as the room id', () => {
-  //   expect(false).toBeTruthy()
-  // })
+  test('removes a child message if streamed in', async () => {
+    const messages = {
+      id1: message,
+      id2: {
+        ...message,
+        uuid: 'id2',
+        parent: message.uuid
+      }
+    }
+    const response = {
+      ...chatResponse,
+      payload: {
+        ...payload,
+        messages
+      }
+    }
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, response)
+    await joinPromise
+    socket.socketClient.emit(ConsumerTopic.ChatRemove, {
+      id: 'roomId',
+      uuid: 'id2',
+      parent: 'id1'
+    })
 
-  // it('does not add the message if the room id is different', () => {
-  //   expect(false).toBeTruthy()
-  // })
+    expect(chat.messages).toStrictEqual([{
+      from: {
+        email: 'foo@bar.com',
+        id: '1',
+        token: 'token',
+        user_type: 'audience',
+        username: 'baz'
+      },
+      message: 'Some text',
+      sent: '2020-09-23T00:00:17.047Z',
+      uuid: 'id1',
+      children: [
+        {
+          from: {
+            email: 'foo@bar.com',
+            id: '1',
+            token: 'token',
+            user_type: 'audience',
+            username: 'baz'
+          },
+          message: 'Some text',
+          sent: '2020-09-23T00:00:17.047Z',
+          uuid: 'id2',
+          parent: 'id1',
+          removed: true
+        }
+      ],
+      removed: false
+    }])
+  })
 
-  // it('does not remove the message if the room id is different', () => {
-  //   expect(false).toBeTruthy()
-  // })
+  test('does not resolve if the get message is not the same as the room id', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, {
+      ...chatResponse,
+      payload: {
+        ...payload,
+        id: 'not the right room id'
+      }
+    })
+    socket.socketClient.emit(ConsumerTopic.ChatGet, {
+      ...chatResponse,
+      payload: {
+        ...payload,
+        messages: {}
+      }
+    })
+    await joinPromise
 
-  // it('does not resolve if the get message is not the same as the room id', () => {
-  //   expect(false).toBeTruthy()
-  // })
+    expect(chat.messages).toStrictEqual([])
+  })
 
-  // it('allows users to send a message', () => {
-  //   expect(false).toBeTruthy()
-  // })
+  test('does not add the message if the room id is different', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, chatResponse)
+    await joinPromise
+    socket.socketClient.emit(ConsumerTopic.ChatReceive, {
+      from: mockUser,
+      id: 'wrong room id',
+      message: 'Next message',
+      sent: '2020-09-24T19:54:05.940Z',
+      uuid: 'id4'
+    })
 
-  // it('throws error if message could not be sent', () => {
-  //   expect(false).toBeTruthy()
-  // })
+    expect(chat.messages).toStrictEqual([{
+      from: {
+        email: 'foo@bar.com',
+        id: '1',
+        token: 'token',
+        user_type: 'audience',
+        username: 'baz'
+      },
+      message: 'Some text',
+      sent: '2020-09-23T00:00:17.047Z',
+      uuid: 'id1',
+      children: [],
+      removed: false
+    }])
+  })
+
+  test('does not remove the message if the room id is different', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, chatResponse)
+    await joinPromise
+    socket.socketClient.emit(ConsumerTopic.ChatRemove, {
+      id: 'wrong room id',
+      uuid: 'id1'
+    })
+
+    expect(chat.messages).toStrictEqual([{
+      from: {
+        email: 'foo@bar.com',
+        id: '1',
+        token: 'token',
+        user_type: 'audience',
+        username: 'baz'
+      },
+      message: 'Some text',
+      sent: '2020-09-23T00:00:17.047Z',
+      uuid: 'id1',
+      children: [],
+      removed: false
+    }])
+  })
+
+  test('allows users to send a message', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, {
+      ...chatResponse,
+      payload: {
+        ...payload,
+        messages: {}
+      }
+    })
+    await joinPromise
+    const promise = chat.sendMessage({
+      message: 'abc'
+    })
+    socket.socketClient.emit(ConsumerTopic.ChatSend, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatSend, {})
+    await promise
+    expect(chat.messages).toStrictEqual([])
+  })
+
+  test('throws error if message could not be sent', async () => {
+    const joinPromise = chat.join()
+    socket.socketClient.emit(ConsumerTopic.ChatGet, initialResponse)
+    socket.socketClient.emit(ConsumerTopic.ChatGet, {
+      ...chatResponse,
+      payload: {
+        ...payload,
+        messages: {}
+      }
+    })
+    await joinPromise
+    const promise = chat.sendMessage({
+      message: 'abc'
+    })
+    socket.socketClient.emit(ConsumerTopic.ChatSend, {
+      error: 'Things went wrong'
+    })
+    try {
+      await promise
+      expect(false).toBeTruthy()
+    } catch (e) {
+      expect(e.message).toBe('Things went wrong')
+    }
+  })
 
   // it('allows users to reply to a message', () => {
   //   expect(false).toBeTruthy()
