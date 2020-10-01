38f5bab520491493113a945123bc2fadb9298d5d
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-10-01 19:53:06 +0100

    chore: start building polls class

---
+++ src/itinerary-item/polls.ts
@@ -0,0 +1,66 @@
+import { ConsumerTopic } from '../topic'
+import { InitialResponse, promiseTimeout } from '../utils'
+
+export interface GetPollsResponse {
+  payload: any
+}
+
+export interface AnswerPollsResponse {
+  payload: any
+}
+
+export interface QuestionAnswerData {
+  question: string;
+  answer: string;
+}
+
+export class Poll {
+  private socket: SocketIOClient.Socket;
+  public id: string;
+  /**
+   *
+   */
+  constructor (socket: SocketIOClient.Socket, id: string) {
+    this.socket = socket
+    this.id = id
+  }
+
+  answer (data: QuestionAnswerData): Promise<void> {
+    let callback: (resp: InitialResponse | AnswerPollsResponse) => void
+    return promiseTimeout(new Promise<void>((resolve, reject) => {
+      callback = (resp: InitialResponse | AnswerPollsResponse) => {
+        if ('error' in resp) {
+          reject(new Error(resp.error))
+        } else if (!('status' in resp)) {
+          resolve()
+        }
+      }
+      this.socket.on(ConsumerTopic.PollAnswer, callback)
+      this.socket.emit(ConsumerTopic.PollAnswer, {
+        id: this.id,
+        data
+      })
+    })).finally(() => {
+      this.socket.removeListener(ConsumerTopic.PollAnswer, callback)
+    })
+  }
+
+  get (): Promise<void> {
+    let callback: (resp: InitialResponse | GetPollsResponse) => void
+    return promiseTimeout(new Promise<void>((resolve, reject) => {
+      callback = (resp: InitialResponse | GetPollsResponse) => {
+        if ('error' in resp) {
+          reject(new Error(resp.error))
+        } else if (!('status' in resp)) {
+          resolve()
+        }
+      }
+      this.socket.on(ConsumerTopic.PollGet, callback)
+      this.socket.emit(ConsumerTopic.PollGet, {
+        id: this.id
+      })
+    })).finally(() => {
+      this.socket.removeListener(ConsumerTopic.PollGet, callback)
+    })
+  }
+}
