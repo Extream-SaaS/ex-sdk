6a8f7a7fbb6e7f5bd68a763782c82ec19599b3cf
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-10-03 20:22:45 +0100

    refactor: move question into its' own file

--- src/itinerary-item/poll.ts
+++ src/itinerary-item/poll.ts
@@ -1,37 +1,20 @@
 import SubscriptionManager from '../subscription-manager'
 import { ClientTopic, ConsumerTopic } from '../topic'
-import { InitialResponse, promiseTimeout, TimeStamp } from '../utils'
+import { InitialResponse, promiseTimeout } from '../utils'
+import { Question, QuestionResponse } from './question'
 
 export enum PollType {
   Timed = 'Timed',
   Immediate = 'Immediate'
 }
 
-export interface AnswerResponse {
-  id: string
-  order: number
-  text: string
-}
-
-export interface QuestionResponse {
-  time?: TimeStamp
-  question: string
-  id: string
-  order: number
-  answers: AnswerResponse[]
-}
-
 export interface GetPollResponsePayload {
   type: PollType
-  questions: QuestionResponse []
+  questions: QuestionResponse[]
 }
 
 export interface GetPollResponse {
   payload: GetPollResponsePayload
-}
-
-export interface AnswerPollsResponse {
-  payload: any
 }
 
 export interface PollListenerResponse {
@@ -53,52 +36,6 @@ export interface PollAnswerResponse {
   data: {
     id: string
     responses: { [key: string]: number }
-  }
-}
-
-export class Question {
-  private socket: SocketIOClient.Socket
-  public id: string
-  public answers: AnswerResponse[]
-  public responses: { [key: string]: number }
-
-  constructor (socket: SocketIOClient.Socket, id: string, data: QuestionResponse) {
-    this.socket = socket
-    this.id = id
-    this.answers = [...data.answers].sort(Question.sortByOrder)
-    // TODO do we get the initial responses from the server or not?
-    this.responses = data.answers.reduce((acc: Record<string, number>, cur: AnswerResponse) => {
-      acc[cur.id] = 0
-      return acc
-    }, {})
-  }
-
-  static sortByOrder (a: AnswerResponse, b: AnswerResponse): number {
-    return a.order - b.order
-  }
-
-  setResponses (responses: { [key: string]: number }): void {
-    this.responses = responses
-  }
-
-  answer (answer: string): Promise<void> {
-    let callback: (resp: InitialResponse | AnswerPollsResponse) => void
-    return promiseTimeout(new Promise<void>((resolve, reject) => {
-      callback = (resp: InitialResponse | AnswerPollsResponse) => {
-        if ('error' in resp) {
-          reject(new Error(resp.error))
-        } else if (!('status' in resp)) {
-          resolve()
-        }
-      }
-      this.socket.on(ConsumerTopic.PollAnswer, callback)
-      this.socket.emit(ConsumerTopic.PollAnswer, {
-        answer,
-        question: this.id
-      })
-    })).finally(() => {
-      this.socket.removeListener(ConsumerTopic.PollAnswer, callback)
-    })
   }
 }
 
