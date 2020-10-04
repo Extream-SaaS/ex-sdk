0f915e1a5d0d06600621d8dac4e57f0f0c2d29f1
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-10-04 20:38:20 +0100

    test: add test for index

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,28 @@
+import { Consumer } from '../consumer'
+import { ExtreamClient } from '../extream-client'
+import * as exSdk from '../index'
+import { Itinerary, ItineraryType } from '../itinerary'
+import { Chat, Poll, PollType, Video } from '../itinerary-item'
+import { AuthorizationTopic, AdminTopic, ClientTopic, ConsumerTopic } from '../topic'
+import { UserType } from '../user'
+
+describe('Index', () => {
+  it('exports extream client as default', () => {
+    expect(exSdk.default).toEqual(ExtreamClient)
+  })
+
+  it('exports other classes as named exports', () => {
+    expect(exSdk.Chat).toEqual(Chat)
+    expect(exSdk.PollType).toEqual(PollType)
+    expect(exSdk.Poll).toEqual(Poll)
+    expect(exSdk.Video).toEqual(Video)
+    expect(exSdk.Consumer).toEqual(Consumer)
+    expect(exSdk.ItineraryType).toEqual(ItineraryType)
+    expect(exSdk.Itinerary).toEqual(Itinerary)
+    expect(exSdk.AuthorizationTopic).toEqual(AuthorizationTopic)
+    expect(exSdk.AdminTopic).toEqual(AdminTopic)
+    expect(exSdk.ClientTopic).toEqual(ClientTopic)
+    expect(exSdk.ConsumerTopic).toEqual(ConsumerTopic)
+    expect(exSdk.UserType).toEqual(UserType)
+  })
+})
