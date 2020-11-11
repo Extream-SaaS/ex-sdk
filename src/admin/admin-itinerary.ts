be7341abeec869deb83593068e3c84577b8b64d3
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-11-11 20:54:38 +0000

    feat: manage itinerary items

--- src/admin/admin-itinerary.ts
+++ src/admin/admin-itinerary.ts
@@ -1,7 +1,26 @@
 import { GetItineraryResponse, ItineraryPayload } from '../event'
 import { AdminTopic, ConsumerTopic } from '../topic'
-import { InitialResponse, promiseTimeout } from '../utils'
+import { InitialResponse, promiseTimeout, SocketResponse } from '../utils'
 import ItineraryItem, { ItineraryItemPayload } from './itinerary-item'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface DeleteItineraryItemResponse {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ChatCreateRequest {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RmtpCreateRequest {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface WebRtcCreateRequest {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PollCreateRequest {}
+
+export type CreateItemRequest = PollCreateRequest | WebRtcCreateRequest | RmtpCreateRequest | ChatCreateRequest
+
+export type CreateItemResponse = SocketResponse<{
+  id: string;
+  type: string;
+}>
 
 export class AdminItinerary {
   private socket: SocketIOClient.Socket
@@ -14,15 +33,11 @@ export class AdminItinerary {
     this.id = id
   }
 
-  createItem (payload: ItineraryPayload): void {
+  create (payload: ItineraryPayload): void {
     const items = payload.items
       ? JSON.parse(payload.items as string)
       : []
-    this.items = items.map((i: ItineraryItemPayload) => {
-      const item = new ItineraryItem(this.socket, i.id)
-      item.createItem(i)
-      return item
-    })
+    this.items = items.map((i: ItineraryItemPayload) => new ItineraryItem(this.socket, i.id, i.type))
     this.data = {
       ...payload
     }
@@ -35,7 +50,7 @@ export class AdminItinerary {
         if ('error' in resp) {
           reject(new Error(resp.error))
         } else if (!('status' in resp)) {
-          this.createItem(resp.payload)
+          this.create(resp.payload)
           resolve()
         }
       }
@@ -55,7 +70,7 @@ export class AdminItinerary {
         if ('error' in resp) {
           reject(new Error(resp.error))
         } else if (!('status' in resp)) {
-          this.createItem(resp.payload)
+          this.create(resp.payload)
           resolve()
         }
       }
@@ -65,6 +80,45 @@ export class AdminItinerary {
       })
     })).finally(() => {
       this.socket.removeListener(ConsumerTopic.ItineraryGet, callback)
+    })
+  }
+
+  deleteItem (id: string): Promise<void> {
+    let callback: (resp: InitialResponse | DeleteItineraryItemResponse) => void
+    return promiseTimeout(new Promise<void>((resolve, reject) => {
+      callback = (resp: InitialResponse | DeleteItineraryItemResponse) => {
+        if ('error' in resp) {
+          reject(new Error(resp.error))
+        } else if (!('status' in resp)) {
+          this.items = this.items.filter(i => i.id === id)
+          resolve()
+        }
+      }
+      this.socket.on(AdminTopic.ItemDelete, callback)
+      this.socket.emit(AdminTopic.ItemDelete, {
+        itinerary: this.id,
+        id
+      })
+    })).finally(() => {
+      this.socket.removeListener(AdminTopic.ItemDelete, callback)
+    })
+  }
+
+  createItineraryItem (item: InitialResponse | CreateItemRequest): Promise<void> {
+    let callback: (resp: InitialResponse | CreateItemResponse) => void
+    return promiseTimeout(new Promise<void>((resolve, reject) => {
+      callback = (resp: InitialResponse | CreateItemResponse) => {
+        if ('error' in resp) {
+          reject(new Error(resp.error))
+        } else if (!('status' in resp)) {
+          this.items = [...this.items, new ItineraryItem(this.socket, resp.payload.id, resp.payload.type)]
+          resolve()
+        }
+      }
+      this.socket.on(AdminTopic.ItemCreate, callback)
+      this.socket.emit(AdminTopic.ItemCreate, item)
+    })).finally(() => {
+      this.socket.removeListener(AdminTopic.ItemCreate, callback)
     })
   }
 }
